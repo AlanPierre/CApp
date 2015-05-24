47c554218206139e3a58b9a6bcf944b5c03dc587
Alan Pierre <alan-pierre@hotmai.com>
2015-05-24 13:24:58 -0300

    CApp

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,14 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 InheritedResources.flash_keys = [ :success, :failure ]
+
+# Include your application configuration below
+Date::MONTHNAMES = [nil] + %w(Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)
+Date::DAYNAMES = %w(Domingo Segunda-Feira Terça-Feira Quarta-Feira Quinta-Feira Sexta-Feira Sábado)
+Date::ABBR_MONTHNAMES = [nil] + %w(Jan Fev Mar Abr Mai Jun Jul Aug Set Out Nov Dez)
+Date::ABBR_DAYNAMES = %w(Dom Seg Ter Qua Qui Sex Sab)
+
+Time::MONTHNAMES = Date::MONTHNAMES
+Time::DAYNAMES = Date::DAYNAMES
+Time::ABBR_MONTHNAMES = Date::ABBR_MONTHNAMES
+Time::ABBR_DAYNAMES = Date::ABBR_DAYNAMES
